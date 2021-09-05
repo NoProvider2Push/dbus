5516075003320fa8aa2999ddafed08a9df788abe
genofire <geno+dev@fireorbit.de>
2021-09-05 19:07:01 +0200

    first step of dbus registration

--- distributor/distributor.go
+++ distributor/distributor.go
@@ -9,25 +9,30 @@ type DBus struct {
 func (d DBus) NewDistributor(distName string) *Distributor {
 	//register on bus
 	return &Distributor{
-		name: distName,
+		client: d,
+		name:   distName,
 	}
 }
 
 type Distributor struct {
-	name string
+	client DBus
+	name   string
 }
 
-func (d Distributor) Register(name, token string) (thing, reason string, err *dbus.Error) {
-
-	return "REGISTRATION_FAILED", "unimpl", nil
+func (d Distributor) Register(appid, token string) (thing, reason string, err *dbus.Error) {
+	c := d.client.NewConnector(appid)
+	if err := c.NewEndpoint(token, "http://.../UP?token="+token); err != nil {
+		return "REGISTRATION_FAILED", err.Error(), nil
+	}
+	return "NEW_ENDPOINT", "", nil
 }
 
 func (d Distributor) Unregister(token string) *dbus.Error {
 	return nil
 }
 
-func (d DBus) NewConector(objstr string) *Connector {
-	obj := d.client.Object(objstr, "/org/unifiedpush/Connector")
+func (d DBus) NewConector(appid string) *Connector {
+	obj := d.client.Object(appid, "/org/unifiedpush/Connector")
 	return &Connector{
 		obj: &obj,
 	}
@@ -38,13 +43,13 @@ type Connector struct {
 }
 
 func (c Connector) Message(token, contents, id string) error {
-	return nil
+	return c.obj.Call("org.unifiedpush.Connector1.Message", dbus.FlagNoReplyExpected, token, contents, id).Err
 }
 
 func (c Connector) NewEndpoint(token, endpoint string) error {
-	return nil
+	return c.obj.Call("org.unifiedpush.Connector1.NewEndpoint", dbus.FlagNoReplyExpected, token, endpoint).Err
 }
 
 func (c Connector) Unregistered(token string) error {
-	return nil
+	return c.obj.Call("org.unifiedpush.Connector1.Unregistered", dbus.FlagNoReplyExpected, token).Err
 }
